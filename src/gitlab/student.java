ec03be979623e501eedaa71286c1633c509110a5
sysadmin <sysadmin@bcalab1-m07>
2021-02-18 10:07:57 +0530

    commit first

--- src/gitlab/student.java
+++ src/gitlab/student.java
@@ -4,6 +4,7 @@ public class student {
 	public static void main(String[] args) {
 	   //To Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("Wassup george");
 		
 		int x;
 		int y;
